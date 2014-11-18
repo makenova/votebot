5310fb41f93251d2a0154599e4aaf70de9a0baac
makenova <themarchmyth@gmail.com>
2014-11-17 22:48:40 -0600

    make it so :joy_cat:

---
+++ server.js
@@ -0,0 +1,58 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = 3000;
+
+app.get('/', function (req, res) {
+  res.send('<h1>Hello World</h1>');
+});
+
+app.get('/index', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/style.css', function (req, res) {
+  res.sendFile(__dirname + '/style.css');
+});
+
+var ballot = {
+  forward: 0,
+  left: 0,
+  right: 0,
+  reverse: 0
+};
+
+function incremetBallot (vote) {
+    ballot[vote]++;
+    console.log(ballot);
+
+    if (ballot[vote] >= 3) {
+      io.emit('consensus', vote);
+      moveBot(vote);
+      Object.keys(ballot).forEach(function (key) {
+        ballot[key] = 0;
+      });
+    }
+    return;
+}
+
+function moveBot(direction) {
+  console.log('moving bot: ' + direction);
+}
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+
+  socket.on('vote', function (msg) {
+    console.log('vote: ' + msg);
+    incremetBallot(msg);
+  });
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(port, function () {
+  console.log('listening on port: ' + port);
+});

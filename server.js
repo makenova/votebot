728fad2cac2f3ed1b3a7f52db6fdbdb3b389a41f
makenova <themarchmyth@gmail.com>
2014-11-24 23:10:46 -0600

    REFACTOR how votes are tallied and ADD comments

--- server.js
+++ server.js
@@ -1,41 +1,66 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+// serve static resources from public dir
+app.use(express.static(path.join(__dirname, 'public')));
+
+// server index page
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + 'public/index.html');
 });
 
-app.get('/style.css', function (req, res) {
-  res.sendFile(__dirname + '/style.css');
-});
-
+// Setup a ballot to hold the votes
 var ballot = {
+  stop: 1,
   forward: 0,
   left: 0,
   right: 0,
   reverse: 0
 };
 
+// every 2 seconds tally the votes
+setInterval(tallyVotes, 2000);
+
+// tally votes, broadcast result and reset ballot
+function tallyVotes () {
+  var max = {vote:'', count:0};
+  for (var vote in ballot) {
+    if (ballot[vote]>=max.count) {
+      max.vote = vote;
+      max.count = ballot[vote];
+    } 
+  }
+
+  moveBot(max.vote);
+  resetBallot();
+}
+
+// reset ballot to 0
+function resetBallot () {
+  Object.keys(ballot).forEach(function (key) {
+    ballot[key] = 0;
+  });
+  // if no one votes, bot should stop
+  ballot['stop'] = 1;
+}
+
+// broadcast result of ballot
+function moveBot(direction) {
+  console.log('moving bot: ' + direction);
+  io.emit('consensus', direction);
+}
+
+// increment the a vote in the ballot
 function incremetBallot (vote) {
     ballot[vote]++;
     console.log(ballot);
-
-    if (ballot[vote] >= 10) {
-      io.emit('consensus', vote);
-      moveBot(vote);
-      Object.keys(ballot).forEach(function (key) {
-        ballot[key] = 0;
-      });
-    }
-    return;
 }
 
-function moveBot(direction) {
-  console.log('moving bot: ' + direction);
-}
-
+// setup messaging for socket.io
 io.on('connection', function (socket) {
   console.log('a user connected');
 
@@ -49,6 +74,7 @@ io.on('connection', function (socket) {
   });
 });
 
+// start server
 http.listen(port, function () {
   console.log('listening on port: ' + port);
 });
--- spark_server.js
+++ spark_server.js
@@ -2,36 +2,39 @@ var five = require("johnny-five");
 var Spark = require("spark-io");
 var socket = require('socket.io-client')('https://okcjs-votebot.herokuapp.com/');
 
+// setup spark core
 var board = new five.Board({
   io: new Spark({
     token: process.env.SPARK_TOKEN,
     deviceId: process.env.SPARK_DEVICE_ID
-  })
+  }),
+  repl: false
 });
 
 board.on("ready", function () {
+  // setup servos
   var left_wheel  = new five.Servo({ pin: "D0", type: 'continuous' }).stop();
   var right_wheel = new five.Servo({ pin: "D1", type: 'continuous' }).stop();
 
+  // respond to move event
   board.on('move', function (dir) {
     console.log(dir);
 
     if (dir == 'forward'){
-      console.log('Forward');
       left_wheel.cw(0.9);
       right_wheel.ccw(0.9);
     } else if (dir == 'reverse') {
-      console.log('Reverse');
       left_wheel.ccw(0.9);
       right_wheel.cw(0.9);
     } else if (dir == 'right') {
-      console.log('Right');
       left_wheel.cw(0.9);
       right_wheel.cw(0.9);
     } else if (dir == 'left') {
-      console.log('Left');
       left_wheel.ccw(0.9);
       right_wheel.ccw(0.9);
+    } else if (dir == 'stop') {
+      left_wheel.stop();
+      right_wheel.stop();
     }
 
   });
@@ -41,6 +44,7 @@ socket.on('connect', function () {
   console.log('connected');
 });
 
+// respond to ballot consensus
 socket.on('consensus', function (dir) {
   board.emit('move', dir);
 });
